f8b1bdf9947e1453c7eb019f48c694e96bb04fe6
HuongLamVu <huonglam4593@gmail.com>
2019-05-01 11:46:57 +0700

    Upload build files

---
+++ precache-manifest.48d6b62e2c4821dddf1edc7f397474cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64942ba7325e241579414e44c4b2d6c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c50d5ed2411856693fcc",
+    "url": "/static/css/main.964d4d49.chunk.css"
+  },
+  {
+    "revision": "1ed8da7f09f755e427ad",
+    "url": "/static/js/2.2ae724df.chunk.js"
+  },
+  {
+    "revision": "c50d5ed2411856693fcc",
+    "url": "/static/js/main.3e855230.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  }
+]);
